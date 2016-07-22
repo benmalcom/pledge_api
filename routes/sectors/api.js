6430bc56a2a8ac0fead9b653cc48e4f2818f0486
benmalcom <benjaminikeji@gmail.com>
2016-07-22 11:17:20 +0100

    rebuild

--- routes/sectors/api.js
+++ routes/sectors/api.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var sectors = require('../../controllers/sectors/sectors.controller');
 var sanitize = require('../../utils/cleaner');
+var config = require('../../config/config');
+
 
 
 
